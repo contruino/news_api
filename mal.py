bc66f7ec425c1fa75e451a3ec24c1cbdeee19e78
Pyprofessor <119044669+contruino@users.noreply.github.com>
2023-05-16 21:59:08 +0500

    Add files via upload

---
+++ mal.py
@@ -0,0 +1,23 @@
+from newsapi import NewsApiClient
+import datetime as dt
+import pandas as pd
+
+newsapi = NewsApiClient(api_key='your api key')
+data = newsapi.get_everything(q='tesla',language='en',page_size=20)
+print(type(data))
+print(data['status'])
+print(data['totalResult'])
+print(data['articles'][0])
+article = data['articles']
+for a,b in enumerate(article):
+    print(f'{a}:     {b["title"]}')
+
+for k,v in article[0].items():
+    print(f'\n{k.just(15)}  {v}')
+
+print(pd.DataFrame(article))
+
+info = newsapi.get_everything(q='cybertruck',from_param='2023-01-31')
+print(info['totalResult '])
+result = data['articles'].copy()
+
